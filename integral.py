3e7e8afc3ea7d2af8c7da175a359982b2a6ba5c0
renardbiemann <miowellers@web.de>
2025-05-18 10:49:01 +0200

    Create integral.py

---
+++ integral.py
@@ -0,0 +1,36 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+def evaluate_function(func, x):
+    try:
+        y = eval(func, {"np": np, "x": x})
+        return y
+    except Exception as e:
+        print(f"Error in function evaluation: {e}")
+        return None
+
+func = input("function:")
+dx = int(input("dx:"))
+a = float(input("a:"))
+b = float(input("b:")) 
+x = np.linspace(a,b,int(dx))
+y = evaluate_function(func, x)
+
+start_time = time.time()
+
+
+for _ in range(10000000):
+    pass
+
+if y is not None:
+    delx = (b - a) / dx
+    vals = y * delx
+    print(sum(vals))
+else:
+    print("Invalid function input.")
+
+end_time = time.time()
+print(f"Execution time: {end_time - start_time:.5f} seconds")
+
